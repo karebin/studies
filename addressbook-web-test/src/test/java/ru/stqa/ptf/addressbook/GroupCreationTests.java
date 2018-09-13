950768c4cd65145c468e3e16a15d306760727598
karebin <kareba91@mail.ru>
2018-09-13 21:39:22 +0300

    fixtura, refactoring test

--- addressbook-web-test/src/test/java/ru/stqa/ptf/addressbook/GroupCreationTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/ptf/addressbook/GroupCreationTests.java
@@ -15,16 +15,16 @@ public class GroupCreationTests {
   public void setUp() throws Exception {
     driver = new FirefoxDriver();
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testGroupCreationTests() throws Exception {
     driver.get("http://localhost/addressbook/index.php");
     driver.findElement(By.name("user")).clear();
     driver.findElement(By.name("user")).sendKeys("admin");
     driver.findElement(By.name("pass")).clear();
     driver.findElement(By.name("pass")).sendKeys("secret");
     driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
+  }
+
+  @Test
+  public void testGroupCreationTests() throws Exception {
     driver.findElement(By.linkText("groups")).click();
     driver.findElement(By.name("new")).click();
     driver.findElement(By.name("group_name")).click();

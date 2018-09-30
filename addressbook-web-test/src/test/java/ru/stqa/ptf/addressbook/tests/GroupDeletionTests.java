72644ebc6dde4b0649edb6b8ca8247984707a765
karebin <kareba91@mail.ru>
2018-09-30 16:06:58 +0300

    Лекция 4.3. Количество элементов в коллекции

--- addressbook-web-test/src/test/java/ru/stqa/ptf/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/ptf/addressbook/tests/GroupDeletionTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.ptf.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.ptf.addressbook.model.GroupData;
 
@@ -11,8 +12,11 @@ public class GroupDeletionTests extends TestBase {
         if (!app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test1", "test1", "test1"));
         }
+        int before = app.getGroupHelper().getGroupCount();
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroup();
         app.getGroupHelper().returnToGroupPage();
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before - 1);
     }
 }
--- addressbook-web-test/src/test/java/ru/stqa/ptf/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/ptf/addressbook/tests/GroupModificationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.ptf.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.ptf.addressbook.model.GroupData;
 
@@ -11,7 +12,10 @@ public class GroupModificationTests extends TestBase {
         if (!app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test1", "test1", "test1"));
         }
+        int before = app.getGroupHelper().getGroupCount();
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().modificationGroup(new GroupData("test2", "test1", "test2"));
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before );
     }
 }

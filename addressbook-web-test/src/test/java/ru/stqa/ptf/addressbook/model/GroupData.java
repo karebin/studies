05c53c77e0ad352cce11e99345e1d8e1eda8a547
karebin <kareba91@mail.ru>
2018-09-30 17:32:33 +0300

    Лекция 4.8. Поиск максимального элемента в коллекции

--- addressbook-web-test/src/test/java/ru/stqa/ptf/addressbook/model/GroupData.java
+++ addressbook-web-test/src/test/java/ru/stqa/ptf/addressbook/model/GroupData.java
@@ -4,12 +4,12 @@ import java.util.Objects;
 
 public class GroupData {
 
-    private final String id;
+    private int id;
     private final String name;
     private final String header;
     private final String footer;
 
-    public GroupData(String id , String name, String header, String footer) {
+    public GroupData(int id , String name, String header, String footer) {
         this.id = id;
         this.name = name;
         this.header = header;
@@ -17,7 +17,7 @@ public class GroupData {
     }
 
     public GroupData(String name, String header, String footer) {
-        this.id = null;
+        this.id = 0;
         this.name = name;
         this.header = header;
         this.footer = footer;
@@ -35,7 +35,7 @@ public class GroupData {
         return footer;
     }
 
-    public String getId() { return id; }
+    public int getId() { return id; }
 
     @Override
     public String toString() {
@@ -58,5 +58,9 @@ public class GroupData {
     public int hashCode() {
 
         return Objects.hash(id, name);
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }

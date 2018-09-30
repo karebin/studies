1c519605b5df08149e333d076d96322c9d7bd73b
karebin <kareba91@mail.ru>
2018-09-30 17:17:40 +0300

    Лекция 4.7. Множества: неупорядоченные коллекции

--- addressbook-web-test/src/test/java/ru/stqa/ptf/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/ptf/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.ptf.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -16,8 +17,12 @@ public class GroupModificationTests extends TestBase {
         }
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
-        app.getGroupHelper().modificationGroup(new GroupData("test2", "test1", "test2"));
+        GroupData group = new GroupData(before.get(before.size()-1).getId(),"test1", "test2", "test3");
+        app.getGroupHelper().modificationGroup(group);
         List <GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
+        before.remove(before.size()-1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 }
